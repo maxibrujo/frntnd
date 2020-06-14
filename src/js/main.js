a8c785d3a651a0f36fadf98b20770fbe8d88f213
maxibrujo <fcb99@mail.ru>
2020-06-14 14:38:45 +0300

    catalog_css_full

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,17 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
-navMain.classList.remove('main-nav--nojs');
-navMain.classList.remove('main-nav--opened');
-navMain.classList.add('main-nav--closed'); 
+navMain.classList.remove('main-nav_nojs');
+navMain.classList.remove('main-nav_opened');
+navMain.classList.add('main-nav_closed'); 
 
 navToggle.addEventListener('click',function(){
-    if (navMain.classList.contains('main-nav--closed')){
-        navMain.classList.remove('main-nav--closed');
-        navMain.classList.add('main-nav--opened');
+    if (navMain.classList.contains('main-nav_closed')){
+        navMain.classList.remove('main-nav_closed');
+        navMain.classList.add('main-nav_opened');
     }
     else {
-        navMain.classList.remove('main-nav--opened');
-        navMain.classList.add('main-nav--closed'); 
+        navMain.classList.remove('main-nav_opened');
+        navMain.classList.add('main-nav_closed'); 
     }
 });
